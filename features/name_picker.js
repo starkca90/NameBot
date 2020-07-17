51232b22113717a08f106a9c1a6984e23a71fd55
Casey Stark <starkca90@gmail.com>
2020-07-17 13:13:37 -0700

    Continue testing refresh

--- features/name_picker.js
+++ features/name_picker.js
@@ -3,14 +3,20 @@ module.exports = function(controller) {
     controller.hears(async (message) => message.text && message.text.toLowerCase() == 'refresh', ['message'], async (bot, message) => {
         await bot.reply(message, 'OK, refreshing room attendees');
 
-        let members = await bot.api.memberships.get(message.channel);
+        try {
+            let members = await bot.api.memberships.get(message.channel);
         
-        for (var i = 0; i < members.length; i++) {
-            console.log(members[i]);
+            for (var i = 0; i < members.length; i++) {
+                console.log(members[i]);
+            }
+            
+        } catch (err) {
+            console.log(err);
         }
-    })
+
+    });
 
     controller.hears(async (message) => message.text && message.text.toLowerCase() == 'select', ['message'], async (bot, message) => {
         await bot.reply(message, 'Pikachu, I choose you!');
-    })
+    });
 }
