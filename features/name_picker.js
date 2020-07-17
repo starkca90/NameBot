7718728218a8eaa2800281bf36097b724e9c045d
Casey Stark <starkca90@gmail.com>
2020-07-17 13:30:59 -0700

    Actually Read Documentation :P

--- features/name_picker.js
+++ features/name_picker.js
@@ -1,22 +1,17 @@
-const webex = require('webex')
-const webex = webex.init({
-    credentials: {
-        access_token: process.env.ACCESS_TOKEN
-    }
-});
-
 module.exports = function(controller) {
     
     controller.hears(async (message) => message.text && message.text.toLowerCase() == 'refresh', ['message'], async (bot, message) => {
         await bot.reply(message, 'OK, refreshing room attendees');
 
         try {
-            let members = await webex.memberships.get(message.channel);
+            
+            let members = await webex.memberships.list({roomId: message.channel})
+            .then(function(memberships) {
+                for (var i = 0; i < members.length; i++) {
+                    console.log(members[i]);
+                }
+            });
         
-            for (var i = 0; i < members.length; i++) {
-                console.log(members[i]);
-            }
-
         } catch (err) {
             console.log(err);
         }
