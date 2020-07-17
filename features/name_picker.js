5853a5a9a2c5e65760dfe64fd15a0cf587249d44
Casey Stark <starkca90@gmail.com>
2020-07-17 13:25:18 -0700

    Changed to dedicated webex npm

--- features/name_picker.js
+++ features/name_picker.js
@@ -1,15 +1,22 @@
+const webex = require('webex')
+const webex = webex.init({
+    credentials: {
+        access_token: process.env.ACCESS_TOKEN
+    }
+});
+
 module.exports = function(controller) {
     
     controller.hears(async (message) => message.text && message.text.toLowerCase() == 'refresh', ['message'], async (bot, message) => {
         await bot.reply(message, 'OK, refreshing room attendees');
 
         try {
-            let members = await bot.api.memberships.get(message.channel);
+            let members = await webex.memberships.get(message.channel);
         
             for (var i = 0; i < members.length; i++) {
                 console.log(members[i]);
             }
-            
+
         } catch (err) {
             console.log(err);
         }
